ad879768b4180cdc82e5381e6bc80ea9f76bdeaa
mtcomadmin <mindtrips.communications@gmail.com>
2015-07-28 18:24:03 +0300

    items fixes

---
+++ impiegato/src/impiegato/util/DuplicateFinder.java
@@ -0,0 +1,37 @@
+package impiegato.util;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DuplicateFinder {
+
+    public static void main(String... args) throws IOException {
+        String[] resources = new String[]{
+            "items/vocabolario.items",
+            "items/nomi.items"
+        };
+
+        for (String resource : resources) {
+            System.out.println("Checking: " + resource);
+            final Set<String> keys = new HashSet<String>();
+            new DataFileReader(resource) {
+
+                @Override
+                protected void processRecord(String record) {
+                    String[] parts = record.split("=");
+                    parts = parts[0].split(",");
+                    String key = parts[0];
+                    // String value = parts[1];
+                    if (!keys.add(key)) {
+                        System.err.println(key);
+                        System.exit(1);
+                    }
+                    // System.out.println(key + "=" + value);
+                }
+            }.process();
+        }
+
+        System.out.println("Done checking for duplicates.");
+    }
+}
